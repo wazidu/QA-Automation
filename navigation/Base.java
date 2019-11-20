b74f76d1a3b369547b8c275bb7bb62f247bd5e6b
Wazid Ullah Murad <wazidullahmurad@gmail.com>
2019-11-21 00:50:32 +0600

    navigation

---
+++ navigation/Base.java
@@ -0,0 +1,19 @@
+
+package selenium.automation.navigation;
+
+import selenium.automation.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+    
+    public static WebDriver getDriver()
+    {
+        System.setProperty("webdriver.chrome.driver", "c:\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return driver;
+    }
+    
+}
---
+++ navigation/Test1.java
@@ -0,0 +1,14 @@
+
+package selenium.automation.navigation;
+
+import selenium.automation.*;
+import org.openqa.selenium.WebDriver;
+
+
+public class Test1 {
+    public static void main(String[] args) {
+        WebDriver driver=Base.getDriver();
+        driver.get("http://google.com");
+    }
+    
+}
